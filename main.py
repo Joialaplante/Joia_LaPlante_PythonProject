3b35b70bed968ce31047616f24e2ee48f2c9208e
Joia LaPlante <joialaplante@Joias-MacBook-Pro.local>
2020-09-08 12:07:09 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+print("Hello my name is Joia")
+print("Welcome to Comp 151")
+age = 5
+# this is a number
+dog_years = 7 * age
+print(f"The dog is {dog_years}")
+fake_years = "123" # this is not a number
+print(f"Fake years is {fake_years}")
+fake_years_2 = fake_years * 7
+print(f"Fake years 2 is {fake_years_2}")
+
